228a26b4600a0985ab9a5283d45241661b87ddd8
AliceYunanTang <tangyunan@hotmail.com>
2017-08-28 21:06:24 +1000

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+const API_KEY = 'c49990ecb5354accb3ed2d56e5ad648a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
